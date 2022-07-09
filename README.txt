9032c263a0a3aada387c8976e31cf5a9dc17fc35
mg751 <mgoreansky@gmail.com>
2022-07-09 14:58:38 +0300

    Add rounds prompt

---
+++ README.txt
@@ -0,0 +1,29 @@
+Outline for rock paper scissors.
+
+A. Create function for computer to
+return rock, paper or scissors at random.
+    1. Create variable result to store randomly generated
+    rock paper or scissors
+    2. Create function to generate random number between 0
+    and 1. If it's less than 0.3333, then rock, if it's between
+    0.3333 and 0.66666, then paper, otherwise scissors.
+    3. Create function that returns result (variable) of
+    the above random calculation
+
+B. Create function to compare user input to computer result.
+    1. Create parameter for user input that converts input
+    to string and all lower case.
+    2. If user input does not match "rock, paper or scissors"
+    break function and return error.
+    3. Within function, perform equality check from user 
+    input to computer result.
+    4. If user input matches computer result, return match
+    string
+    5. If user input is rock and cResult is paper ...
+    6. If user input is rock and cResult is scissors ...
+    7. Return  
+
+C. Rounds. 
+    1. Create function that calls game() function for an X 
+    number of times
+    2. I got lazy and started experimenting rather than writing
--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function getRandom() {
     return Math.random();
 }
@@ -22,26 +17,36 @@ function playerInput() {
         
         if (["rock","paper","scissors"].includes(lowercaseAnswer)) {
             return lowercaseAnswer;
-        } else return alert("Please enter rock, paper or scissors");
+        } else return "Please type rock, paper or scissors";
     }
 
-    function game(){
-        let computerPlay = computerCalculation();
-        let humanPlay = playerInput();
+function game(){
+    let computerPlay = computerCalculation();
+    let humanPlay = playerInput();
         if (computerPlay == humanPlay){
-            return `Draw. Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Draw. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
         } else if (computerPlay == "rock" && humanPlay == "scissors"){
-            return `Lose. Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Lose. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
         } else if (computerPlay == "paper" && humanPlay == "scissors"){
-            return `Win. Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Win. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
         } else if (computerPlay == "rock" && humanPlay == "paper"){
-            return `Win. Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Win. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
         } else if (computerPlay == "paper" && humanPlay == "rock"){
-            return `Lose. Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Lose. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
         } else if (computerPlay == "scissors" && humanPlay == "rock"){
-            return `Win Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Win. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
         } else if (computerPlay == "scissors" && humanPlay == "paper"){
-            return `Lose. Human chose ${humanPlay}, Computer chose ${computerPlay}`;
+            return `Lose. Human chose ${humanPlay}, Computer chose ${computerPlay}!`;
+        }   else return(humanPlay)
+        }
         
+
+
+function fullGame(rounds=5){
+            rounds = prompt("How many rounds?", "Enter a number")
+            for (let i = 0; i < rounds; i++) {
+                console.log(game());
         }
-        }
+}
+
+fullGame();
